3fb9e48dafdebe3e29908fe7ff368bdc1122ecd5
Roberto Victor Silva Bezerra <roberto.bezerra@g.globo>
2025-06-07 02:44:32 -0300

    Common 1920 screen size adaptation

--- src/interface.py
+++ src/interface.py
@@ -1,19 +1,21 @@
 import time
-from datetime import datetime
+import os
 import pyautogui
-import pywhatkit as kit
 import pyperclip
 import querier
+
+# TO-DO:
+# - Workaround sobre janela de notificações.
 
 def monitor_message():
     placeholder = ""
     while True:
         time.sleep(5)
-        x1, y1 = [300,-600]
-        x2, y2 = [1770,-144] # Apenas durante os testes
-        #x2, y2 = [745,-144]
-        x3, y3 = [1635,-386] # Apenas durante os testes
-        #x3, y3 = [831,-434]
+        x1, y1 = [235,195] # Último contato
+        x2, y2 = [1605,953] # Última mensagem (Apenas durante os testes / automensagem)
+        #x2, y2 = [675,947] # Última mensagem
+        x3, y3 = [1660,640] # Opção de cópia, no menu de contexto (Apenas durante os testes / automensagem)
+        #x3, y3 = [730,635] # Opção de cópia, no menu de contexto
 
         pyautogui.moveTo(x1,y1)
         pyautogui.click()
@@ -31,28 +33,24 @@ def monitor_message():
             process_message(text)
         print(". \n")
     
-
 def process_message(text):
     instruction = "Traduza a mensagem a seguir para espanhol, responda apenas com a tradução: "
     message = instruction + text
     result = querier.send_message(message)
-    reply_message("+5521982736561",result)
+    reply_message(result)
 
-def reply_message(phone_number, message):
-    #hour = datetime.now().hour
-    #minute = datetime.now().minute + 2
-    #kit.sendwhatmsg(phone_number, message, time_hour=hour, time_min=minute)
-
-    x4, y4 = [800,-80]
+def reply_message(message):
+    x4, y4 = [800,-80] # Caixa de entrada de texto
     pyautogui.moveTo(x4,y4)
     pyautogui.click()
     pyautogui.typewrite(message)
     pyautogui.press("enter")
 
-monitor_message()
-
-
-
+if __name__ == "__main__":
+    path = r"C:\Users\Roberto\Desktop\WhatsApp.lnk"
+    os.system(path)
+    time.sleep(5)
+    monitor_message()
 
 
 
