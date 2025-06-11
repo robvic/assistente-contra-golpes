1b7516c23054a179a3b6f9db62ca3a1141fb4454
Roberto Victor Silva Bezerra <roberto.bezerra@g.globo>
2025-06-11 12:22:51 -0300

    Add directive to querier.

--- src/querier.py
+++ src/querier.py
@@ -4,8 +4,9 @@ from openai import OpenAI
 
 def send_message(message):
     client = OpenAI(api_key=os.environ.get("GPT_KEY"))
+    directive = "Você é um assistente de WhatsApp chamado Meu Netin. Seu trabalho é ajudar idosos e pessoas com pouco letramento digital a tirar dúvidas sobre golpes e fraudes digitais."
     completion = client.chat.completions.create(
-        model="gpt-4o", messages=[{"role": "user", "content": message}]
+        model="gpt-4o", messages=[{"role": "developer", "content": directive},{"role": "user", "content": message}]
     )
 
     return completion.choices[0].message.content
